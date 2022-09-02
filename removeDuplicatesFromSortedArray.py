347da2b760baae1609150538ac0dc72abccf9870
Nathan Harding <nathanharding02@gmail.com>
2022-09-01 20:44:03 -0600

    9.1.22

--- removeDuplicatesFromSortedArray.py
+++ removeDuplicatesFromSortedArray.py
@@ -1,10 +1,10 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        l = 1
+        l = 1 # start by pointing at second node since first is unique
 
         for r in range(1, len(nums)):
-            if nums[r] != nums[r-1]:
-                nums[l] = nums[r]
-                l += 1
+            if nums[r] != nums[r-1]: # if pointer value is not equal to the value to its left
+                nums[l] = nums[r] # set pointer to that value
+                l += 1 # increment to next spot in array
 
         return l
